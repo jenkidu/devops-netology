e6986a502b39c1befd66121b7851d5e830db6a0c
Alexey Gladyshev <jenkidu@yandex.ru>
2021-11-18 15:51:08 +0000

    git-rebase 2 commitn

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,9 +3,14 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
 =======
     count=$(( $count + 1 ))
+=======
+   echo "Next parameter: $param"
+   count=$(( $count + 1 ))
+>>>>>>> 7ca5030... git-rebase 2
 done
 
 echo "====="
